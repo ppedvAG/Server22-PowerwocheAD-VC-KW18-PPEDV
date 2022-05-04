4ee1340dfd8419c9a5a502d2eba80c09442bf682
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-05-04 07:36:26 +0200

    Samples1

---
+++ SRV2022-AD/FSMO.ps1
@@ -0,0 +1,16 @@
+﻿#PDC Emulator (0)
+#RID Pool Manager (1)
+#Infrastruktur Master (2)
+#Domain Naming Master (3)
+#Schema-Master (4)
+
+(Get-ADDomainController).OperationMasterRoles
+
+
+Move-ADDirectoryServerOperationMasterRole -Identity <Ziel-DC> -OperationMasterRole SchemaMaster,RIDMaster,InfrastructureMaster,DomainNamingMaster,PDCEmulator
+
+
+Move-ADDirectoryServerOperationMasterRole -Identity <Ziel-DC> -OperationMasterRole 0,1,2,3,4
+
+
+Move-ADDirectoryServerOperationMasterRole -Identity <Ziel-DC> -OperationMasterRole 0,1,2,3,4 -Force
